04fa0c0ab0c340795130a17fe4ee0cefa9535f1a
n1t5ua5 <nitsua5@proton.me>
2024-09-24 02:20:00 -0400

    created java journal

---
+++ javaProjects/is.java
@@ -0,0 +1,103 @@
+// SEPTEMBER 2024
+
+
+
+/*
+Even / Odd: https://github.com/n1t5ua5/pbsc/blob/main/javaProjects/evenodd.java
+
+I used the Scanner class to accept user input from the console. The program then prompts the user to enter an integer & reads
+the input using the nextInt() method of the Scanner object. Once the integer is input, the program calls the isEven() method.
+Inside this method, it checks whether the number is divisible by 2 by using the modulus operator (%). If the remainder when
+divided by 2 is zero, the method returns true, otherwise, it returns false i.e. odd. The returned boolean value from the
+isEven() method is stored in the isEvenNumber variable.
+ */
+
+
+
+/*
+Quarts to Gallons: https://github.com/n1t5ua5/pbsc/blob/main/javaProjects/quartstogallons.java
+
+I defined a constant QUART_IN_GALLON, which holds the value of quarts in one gallon (4). The program sets an
+initial value of quartsNeeded (18) & calculates the number of full gallons by dividing quartsNeeded by QUART_IN_GALLON.
+Then the program prints a message displaying how many full gallons & extra quarts are needed for a job requiring 18 quarts.
+ */
+
+
+
+/*
+Circle Calculator: https://github.com/n1t5ua5/pbsc/blob/main/javaProjects/circle.java
+
+First I defined a class called Circle. The class provides a constructor that initializes the radius, along with getter &
+setter methods to access & modify the radius. It also includes methods to calculate the area & circumference of the circle.
+The area is calculated using the formula π * r² & the circumference is calculated using 2 * π * r. In the main method,
+an instance of Circle is created with an initial radius of 5 & its radius, area & circumference are printed. After,
+the radius is updated to 7 & the updated attributes are displayed once more using the System.out.println() method.
+ */
+
+
+
+/*
+Person Data: https://github.com/n1t5ua5/pbsc/blob/main/javaProjects/Person.java
+
+This program defines a Person class that models a person with two attributes: name & age. The class includes
+a constructor that initializes these attributes, along with getter methods to retrieve someone’s name & age.
+The main method creates two instances of the Person class, person1 & person2, & calls the
+displayPersonInfo() method for each.
+ */
+
+
+
+/*
+Rectangle Calculator: https://github.com/n1t5ua5/pbsc/blob/main/javaProjects/Rectangle.java
+
+The Rectangle class defines a rectangle with width & height attributes. The program includes a constructor
+to initialize these attributes, & two methods to calculate the rectangle's area (calculateArea()) & perimeter
+(calculatePerimeter()). In the main method, an instance of Rectangle is created with a width of 5.0 & height
+of 3.0. Finally, the program calculates & displays the area & perimeter of the rectangle.
+ */
+
+
+
+/*
+Tables & Chairs: https://github.com/n1t5ua5/pbsc/blob/main/javaprojects/tablesandchairs.java
+
+In this simple Java program, the TableAndChairs class is designed to print a visual representation
+of a table & chairs using. The program uses the System.out.println() method to print several lines
+of text that, when combined, form the shape of a table with chairs on either side. The output visually
+mimics the arrangement of chairs ("X") on the sides of a table ("XXXXXXXXXX") with legs ("X") on both ends.
+ */
+
+
+
+/*
+Dog Info: github.com/n1t5ua5/pbsc/blob/main/javaProjects/Dog.java
+
+First, I defined a class that represents a dog with two attributes: name & breed. The class includes a constructor to
+initialize these attributes, as well as getter & setter methods to access & modify them. It also features a method
+to display the dog's details in a formatted manner. In the main method, two instances of the Dog class are created,
+representing different dogs. The program first prints their initial attributes, then updates their names & breeds
+using the setter methods. Finally, I printed the updated information.
+ */
+
+
+
+/*
+Song Lyrics: https://github.com/n1t5ua5/pbsc/blob/main/javaProjects/SongLyrics.java
+
+SongLyrics, is a program that prints four lines from the lyrics of the song "A World on Fire" by the band Within the Ruins.
+Each line is displayed using the System.out.println() method. Additionally, the program includes a link to the song on
+YouTube, allowing users to access the full track easily.
+
+ */
+
+
+
+/*
+Movie Quote Info: https://github.com/n1t5ua5/pbsc/blob/main/javaprojects/moviequoteinfo.java
+
+MovieQuoteInfo is another straightforward Java program that presents a series of quotes from various films.
+A public class is defined called MovieQuoteInfo. Then each quote is displayed along with the character who
+said it, the movie's title & its release year, all formatted into a single string for clarity within four
+individual print statements.
+
+ */
