49ef33cd72a2e6aa72866f9734e5d5ba9d49d4bd
n1t5ua5 <nitsua5@proton.me>
2024-09-24 21:15:27 -0400

    fixed logic

--- javaProjects/Percentages.java
+++ javaProjects/Percentages.java
@@ -2,20 +2,19 @@ import java.util.Scanner;
 
 public class Percentages {
 
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         // Prompt user to enter the double values
         System.out.println("Enter the first number: ");
         double num1 = input.nextDouble();
 
-        System.out.print ("Enter the second number: ");
+        System.out.print("Enter the second number: ");
         double num2 = input.nextDouble();
 
         // Call the computePercent() method to compute & display percent
         computePercent(num1, num2);
         // Call the method again with values in reverse order
-        computePercent(num1, num2);
+        computePercent(num2, num1);
 
         // Close scanner
         input.close();
