07fdf232d9ba0124b76c77afe932b18ad107d3be
n1t5ua5 <nitsua5@proton.me>
2024-09-24 21:05:17 -0400

    percent project added

--- javaProjects/Percentages.java
+++ javaProjects/Percentages.java
@@ -1,1 +1,31 @@
-import java
+import java.util.Scanner;
+
+public class Percentages {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Prompt user to enter the double values
+        System.out.println("Enter the first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print ("Enter the second number: ");
+        double num2 = input.nextDouble();
+
+        // Call the computePercent() method to compute & display percent
+        computePercent(num1, num2);
+        // Call the method again with values in reverse order
+        computePercent(num1, num2);
+
+        // Close scanner
+        input.close();
+    }
+
+    // Method to compute & display percentage
+    public static void computePercent (double a, double b) {
+        double percentage = (a / b) * 100;
+        System.out.printf ("%.2f is %.2f%% of %.2f%n", a, percentage, b);
+
+    }
+
+}
