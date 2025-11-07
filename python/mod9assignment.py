c4cbd8cbff2a27a843d969597ed31bf6439a4635
n1t5ua5 <110934299+n1t5ua5@users.noreply.github.com>
2025-11-07 22:34:08 +0000

    now completed officially

--- python/mod9assignment.py
+++ python/mod9assignment.py
@@ -22,13 +22,13 @@ try:
     carsAvailable = int(input('Please enter the total number of cars available: '))
     rentalTotal = int(input('Please enter the total number of cars you want to rent: '))
 
-        if carsAvailable == 0:
-            raise ValueError('Error: The number of available cars cannot be zero.')
+    if carsAvailable == 0:
+        raise ValueError('Error: The number of available cars cannot be zero.')
 
-        if rentalTotal > carsAvailable:
-            raise SyntaxError('Error: You cannot enter a rental total that is greater than the number of cars available.')
+    if rentalTotal > carsAvailable:
+        raise SyntaxError('Error: You cannot enter a rental total that is greater than the number of cars available.')
 
-        updatedCount = carsAvailable - rentalTotal
+    updatedCount = carsAvailable - rentalTotal
 
 except ValueError as e:
     print(f"\nError: {e}")
